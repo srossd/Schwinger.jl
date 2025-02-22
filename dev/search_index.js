201204343c5d58c1a1188ba87570b861fc627b4b
Documenter.jl <documenter@juliadocs.github.io>
2025-02-22 06:58:43 +0000

    build based on e44a571

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#Schwinger.jl","page":"Index","title":"Schwinger.jl","text":"","category":"section"},{"location":"index.html","page":"Index","title":"Index","text":"Documentation for Schwinger.jl","category":"page"}]
+}
